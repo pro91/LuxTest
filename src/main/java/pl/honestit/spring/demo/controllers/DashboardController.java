d2901d4277df8e7d49ab961bd744bdb0c670b6f8
Rosa <pr.rosinski@gmail.com>
2019-09-15 10:35:33 +0200

    dashboard

---
+++ src/main/java/pl/honestit/spring/demo/controllers/DashboardController.java
@@ -0,0 +1,15 @@
+package pl.honestit.spring.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dashboard")
+public class DashboardController {
+
+    @GetMapping
+    public String DashboardPage() {
+        return "dashboard-page";
+    }
+}
